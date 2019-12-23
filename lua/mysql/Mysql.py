19114eac06832af50e5b709d4eb31684666bfd42
mahongtao <285182060@qq.com>
2019-12-23 10:44:18 +0800

    连接mysql

--- lua/mysql/Mysql.py
+++ lua/mysql/Mysql.py
@@ -7,7 +7,9 @@
 import mysql.connector
 
 # 注意把password设为你的root口令:
-conn = mysql.connector.connect(user='root', password='password', database='test')
+conn = mysql.connector.connect(host='127.0.0.1', port='3306', user='root', password='123456', database='pig')
+
+# conn = mysql.connector.connect(user='root', password='123456', database='pig')
 cursor = conn.cursor()
 # 创建user表:
 cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
